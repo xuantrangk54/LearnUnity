9250bac6983d9bb65060a9e785f2a0ee8244aaa4
xuantrangk54 <xuantrangk54@gmail.com>
2024-09-28 20:36:14 +0700

    lession-2: roate shapre

--- Assets/SampleRotatesWithDeltaTime.cs
+++ Assets/SampleRotatesWithDeltaTime.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameObject : MonoBehaviour
+public class SampleRotatesWithDeltaTime : MonoBehaviour
 {
+    public float degreesPerSecond = 90.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log ("This is the first time i learn Unity");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"DeltaTime is: {Time.deltaTime}");
+        transform.Rotate(0, 0, degreesPerSecond * Time.deltaTime);
     }
 }
